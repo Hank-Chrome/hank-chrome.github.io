784d6d080fd6eb5cde5b7b275e8f509cc1666c5d
Hank-Chrome <116157191+Hank-Chrome@users.noreply.github.com>
2023-01-20 19:28:43 +0800

    Create .jslgjs.js

---
+++ moe-cdn.fly3949.com/资产/.jslgjs.js
@@ -0,0 +1,43 @@
+//这是一个垃圾js脚本
+hfhhehdhehehe
+diiiiiiiiiiiiiiiiiiiiiiii\
+python js
+
+
+js.js
+jjswi
+
+
+
+.html
+讨厌html
+h5
+1给哈哈哈哈
+垃圾js
+//这是一个垃圾js]\
+jsjsjsj
+jnsjj 
+document.write("<script language=javascript src='./renderer.js'></script>"); test('click', 'asynchronous message', 'ping');
+function addScript(url){
+	document.write("<script language=javascript src="+url+"></script>");
+}
+function addScript(url){
+	document.write("<script language=javascript src="+url+"></script>");
+}
+//定义数组
+var msgArray = [
+	{name: 'In function',value: '在函数'},
+	{name: 'was not declared in this scope',value: '没有在此范围内声明'},
+];
+
+//定义常量
+var messageText = {
+
+	a_name : '张三',
+
+	b_name : '李四',
+
+}
+exports.msgArray = msgArray;
+exports.messageText = messageText;
+start lg.js
